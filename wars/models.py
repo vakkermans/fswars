5a4c45564e4e0e05f07ad48fb1ecb8a1d37951f0
Vincent Akkermans <vincent.akkermans@gmail.com>
2011-06-17 00:18:35 +0200

    added battle class

--- wars/models.py
+++ wars/models.py
@@ -2,9 +2,28 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+import json
 
-class FSWUser(models.Model):
-    nickname            = models.CharField(max_length=32, db_index=True, unique=True)
-    sounds              = models.CharField(max_length=1024, db_index=False)
-    player_number       = models.IntegerField(default=0)
-    modified            = models.DateTimeField(auto_now=True)
+#class FSWUser(models.Model):
+#    nickname            = models.CharField(max_length=32, db_index=True, unique=True)
+#    sounds              = models.CharField(max_length=1024, db_index=False)
+#    player_number       = models.IntegerField(default=0)
+#    modified            = models.DateTimeField(auto_now=True)
+
+class Battle(models.Model):
+    player1             = models.CharField(max_length=32)
+    player2             = models.CharField(max_length=32, null=True)
+    player1_sounds      = models.CharField(max_length=512, null=True) # json dumped list of ints
+    player2_sounds      = models.CharField(max_length=512, null=True)
+    turn_owner          = models.IntegerField()
+    history             = models.CharField(max_length=2048) # json dumped history
+    finished            = models.BooleanField()
+
+    def status_players(self):
+        return self.player1 and self.player2
+
+    def status_sounds(self):
+        return self.player1_sounds and self.player2_sounds
+
+    def status(self):
+        return json.dumps(self)
